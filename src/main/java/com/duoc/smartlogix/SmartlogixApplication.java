69efa4f5cb1e835056feecfc00d232383e2300d0
lBlackDev <rolando3365@gmail.com>
2026-03-21 11:33:26 -0300

    Primer Commit

---
+++ src/main/java/com/duoc/smartlogix/SmartlogixApplication.java
@@ -0,0 +1,13 @@
+package com.duoc.smartlogix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartlogixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartlogixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/duoc/smartlogix/SmartlogixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duoc.smartlogix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartlogixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
